91eb6f951a4c63a1e84c87312748c08cf41dd6f5
Petro Prykhodko <petro.prykhodko2004@gmail.com>
2026-05-25 15:01:55 +0200

    feat(clinic): add new models Doctor, Patient, Appointment, CTScan

--- clinic/models.py
+++ clinic/models.py
@@ -1,3 +1,65 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Doctor(AbstractUser):
+    specialization = models.CharField(max_length=255)
+    years_of_experience = models.PositiveIntegerField(default=0)
+    license_number = models.CharField(max_length=20, unique=True)
+
+    class Meta:
+        ordering = ["first_name", "last_name"]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Patient(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    birth_date = models.DateField()
+    phone_number = models.CharField(max_length=20, unique=True)
+    doctors = models.ManyToManyField(
+        Doctor,
+        related_name="patients"
+    )
+
+    class Meta:
+        ordering = ["first_name", "last_name"]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(
+        Doctor,
+        on_delete=models.CASCADE,
+        related_name="appointments"
+    )
+    patient = models.ForeignKey(
+        Patient,
+        on_delete=models.CASCADE,
+        related_name="appointments"
+    )
+    appointment_date = models.DateTimeField()
+    notes = models.TextField(max_length=255, blank=True, default="-")
+
+    class Meta:
+        ordering = ["appointment_date"]
+
+    def __str__(self):
+        return f"({self.appointment_date:%d.%m.%Y}) {self.patient}"
+
+
+class CTScan(models.Model):
+    patient = models.ForeignKey(
+        Patient,
+        on_delete=models.CASCADE,
+        related_name="ct_scans"
+    )
+    scan_date = models.DateTimeField()
+    description = models.TextField(max_length=255, blank=True, default="-")
+
+    class Meta:
+        ordering = ["scan_date"]
