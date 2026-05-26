f8dc6b7b13bc03b577dbc8a4a84a3b964597f03d
Petro Prykhodko <petro.prykhodko2004@gmail.com>
2026-05-26 18:08:30 +0200

    feat(clinic): register models in admin panel

--- clinic/admin.py
+++ clinic/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from clinic.models import Doctor, Patient, Appointment, CTScan
+
+
+@admin.register(Doctor)
+class DoctorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("specialization", )
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("specialization", "years_of_experience", )}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (("Additional info", {"fields": ("specialization", "years_of_experience",)}),)
+    )
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "birth_date", "phone_number", )
+    search_fields = ("first_name", "last_name")
+
+    @admin.display(description="Patient")
+    def full_name(self, obj):
+        return str(obj)
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ("patient", "doctor", "appointment_date", "notes")
+
+
+@admin.register(CTScan)
+class CTScanAdmin(admin.ModelAdmin):
+    pass
