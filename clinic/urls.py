77b987c9263a3a6353a99467968bddaecf9a7c78
Petro Prykhodko <petro.prykhodko2004@gmail.com>
2026-05-28 13:23:56 +0200

    feat(clinic): update index counters and add DoctorListView

--- clinic/urls.py
+++ clinic/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from clinic.views import index, DoctorListView, PatientListView
+from clinic.views import index, DoctorListView
 
 urlpatterns = [
     path("", index, name="index"),
     path("doctors/", DoctorListView.as_view(), name="doctor-list"),
-    path("patients/", PatientListView.as_view(), name="patient-list"),
 ]
 
 app_name = "clinic"
--- clinic/views.py
+++ clinic/views.py
@@ -1,6 +1,26 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
+
+from clinic.models import Doctor, Patient, Appointment, CTScan
 
 
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello, world.")
+    num_doctors = Doctor.objects.count()
+    num_patients = Patient.objects.count()
+    num_appointments = Appointment.objects.count()
+    num_images = CTScan.objects.count()
+    context = {
+        "num_doctors": num_doctors,
+        "num_patients": num_patients,
+        "num_appointments": num_appointments,
+        "num_images": num_images,
+    }
+    return render(request, "clinic/index.html", context=context)
+
+
+class DoctorListView(LoginRequiredMixin, generic.ListView):
+    model = Doctor
